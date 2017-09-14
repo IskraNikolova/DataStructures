868afc0c5438af0d9d709de1660a610d0f892eb3
IskraNikolova <air_iskra@yahoo.com>
2017-09-14 14:53:59 +0300

    update

--- Homework Algorithm Complexity and Linear Data Structures/06 Rreversed List/TestList.cs
+++ Homework Algorithm Complexity and Linear Data Structures/06 Rreversed List/TestList.cs
@@ -6,18 +6,6 @@ namespace _06_Rreversed_List
     {
         public static void Main()
         {
-            ReveresedList<int> testInt = new ReveresedList<int>();
-            testInt.Add(1);
-            testInt.Add(2);
-            testInt.Add(3);
-            testInt.Add(4);
-            testInt.Add(5);
-            testInt.RemoveAt(2);
-            foreach (var el in testInt)
-            {
-                Console.WriteLine(el);
-            }
-         
         }
     }
 }
