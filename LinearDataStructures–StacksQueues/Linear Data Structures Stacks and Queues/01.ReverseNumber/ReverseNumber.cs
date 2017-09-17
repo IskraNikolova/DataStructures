ede379d8aa4aaa82916f68a13fc1f33a4ee59500
IskraNikolova <air_iskra@yahoo.com>
2017-09-17 11:58:34 +0300

    added new project

---
+++ LinearDataStructures–StacksQueues/Linear Data Structures Stacks and Queues/01.ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ReverseNumber
+{
+    public class ReverseNumber
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse);
+            Stack<int> inputs = new Stack<int>();
+            foreach (var el in input)
+            {
+                inputs.Push(el);
+            }
+
+            foreach (var el in inputs)
+            {
+                Console.Write(el + " ");
+            }
+        }
+    }
+}
---
+++ LinearDataStructures–StacksQueues/Linear Data Structures Stacks and Queues/02.CalculateSequenceWithAQueue/CalculateSequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.CalculateSequenceWithAQueue
+{
+    public class CalculateSequence
+    {
+        public static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            Queue<int> sequances = new Queue<int>();
+            sequances.Enqueue(input);
+            int index = 1;
+            List<int> result = new List<int>();
+            while (index <= 50)
+            {
+                var firstElement = sequances.Dequeue();
+                result.Add(firstElement);
+                var second = firstElement + 1;
+                var third = firstElement * 2 + 1;
+                var fourth = firstElement + 2;
+                sequances.Enqueue(second);
+                sequances.Enqueue(third);
+                sequances.Enqueue(fourth);
+                index++;
+            }
+
+            Console.WriteLine(String.Join(", ", result));
+        }
+    }
+}
